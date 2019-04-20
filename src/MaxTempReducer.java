b830fbf7bc6afe521a02f2418dcbede355333678
Rizal Alfarizi <ijal.alfarizi@gmail.com>
2019-04-20 20:38:52 +0700

    add comment

--- src/MaxTempReducer.java
+++ src/MaxTempReducer.java
@@ -6,12 +6,15 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class MaxTempReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-
+        //looping grup dari temperatur
+        //inisialisasi awal maximum temperatur
         int maxVal = Integer.MIN_VALUE;
         for(IntWritable val : values){
+            //cek yang mana paling besar antar value iterasi dan variabel max
             maxVal = Math.max(maxVal,val.get());
         }
-
+        //output dikembalikan ke context
+        //contoh: {1901, 32}
         context.write(key,new IntWritable(maxVal));
     }
 }
